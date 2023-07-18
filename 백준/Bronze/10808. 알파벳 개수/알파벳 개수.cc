1e734de141a86e0501bce76377421c891663ffe1
yusunwoo <115936166+yusunwoo@users.noreply.github.com>
2023-07-18 20:13:49 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int cnt[26];
+int main(){
+    string s;
+    cin >> s;
+    for(char a : s) cnt[a-'a']++;
+    for(int i = 0; i < 26; ++i) cout << cnt[i] << " ";
+}
