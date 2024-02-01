9dd9a148b5e95340b2da9f4f8be7ffb177337561
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-02-02 01:54:00 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.cc
+++ 백준/Silver/4375. 1/1.cc
@@ -1,22 +1,20 @@
 #include<bits/stdc++.h>
-using namespace std;
-int n;
-typedef long long ll;
+using namespace std; 
+int n, cnt, ret;
 int main(){
     ios_base::sync_with_stdio();
     cin.tie(NULL); cout.tie(NULL);
-    while(cin >> n){
-        int cnt = 1, ret = 1;
-        while(1){
-            if(cnt%n == 0){
-                cout << ret << "\n";
+    while (cin >> n){
+        cnt = 1;
+        ret = 1;
+        while (1){
+            if((ret %= n) == 0){
+                cout << cnt << "\n";
                 break;
-            }else{
-                cnt = (cnt * 10) + 1;
-                cnt %= n;
-                ret++;
             }
+            ++cnt;
+            ret *= 10;
+            ++ret;
         }
     }
-    return 0;
 }
