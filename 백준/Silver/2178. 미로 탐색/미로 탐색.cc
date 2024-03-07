594cf9916789da8e31ec993e670e22bc418db641
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-03-07 13:57:48 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,34 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-int N, M, x, y;
-int dy[4] = {-1,0,1,0};
-int dx[4] = {0,1,0,-1};
-int adj[104][104];
-int visited[104][104];
+int n, m, cnt, y, x, ny, nx;
+int dy[4] = {-1, 0, 1, 0};
+int dx[4] = {0, 1, 0, -1};
+int adj[100][100];
+int visited[100][100];
 queue<pair<int, int>> q;
-void bfs(int sy, int sx){
-    visited[sy][sx] = 1;
-    q.push({sy,sx});
-    while (q.size()){
-        tie(y, x) = q.front(); q.pop();
-        for(int i =0;i<4;++i){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || ny >=N || nx < 0 || nx >= M || adj[ny][nx] == 0) continue;
-            if(visited[ny][nx]) continue;
-            visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
-        }
-    }
-    return;
-}
 int main(){
-    cin >> N >> M;
-    for(int i = 0;i<N;++i){
-        for(int j=0;j<M;++j)
+    cin >> n >> m;
+    for(int i = 0; i < n; ++i){
+        for(int j = 0; j < m; ++j)
             scanf("%1d", &adj[i][j]);
     }
-    bfs(0, 0);
-    cout << visited[N-1][M-1] << "\n";
-    return 0;
+    visited[0][0] = 1;
+    q.push({0, 0});
+    while(q.size()){
+        tie(y, x) = q.front();
+        q.pop();
+        for(int i = 0; i <4; ++i){
+            ny = y + dy[i];
+            nx = x + dx[i];
+            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+            if(adj[ny][nx] && !visited[ny][nx]){
+                visited[ny][nx] = visited[y][x] + 1;
+                q.push({ny, nx});
+            }
+        }
+    }
+    cout << visited[n-1][m-1] << "\n";
 }
