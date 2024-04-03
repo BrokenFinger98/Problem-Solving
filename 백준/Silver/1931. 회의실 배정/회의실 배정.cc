9573d9d37e8336de10e28c42272033bf6c06bf15
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-04-03 23:18:09 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int from, to, n, ret = 1;
+vector<pair<int, int>> v;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; ++i){
+        cin >> from >> to;
+        v.push_back({to, from});
+    }
+    sort(v.begin(), v.end());
+    from = v[0].second;
+    to = v[0].first;
+    for(int i = 1; i < n; ++i){
+        if(v[i].second < to) continue;
+        to = v[i].first; from = v[i].second; ++ret;
+    }
+    cout << ret << "\n";
+    return 0;
+}
