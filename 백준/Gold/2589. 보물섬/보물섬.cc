ff0503a965dfbc87a6ae1bbe7ddc6844cade90cb
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-04-11 16:37:12 +0900

    [Gold V] Title: 보물섬, Time: 112 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,51 +1,47 @@
 #include<bits/stdc++.h>
-using namespace std;
-int n, m, y, x, ny, nx, ret = 0;
-char c;
-int adj[50][50];
+using namespace std; 
+int n, m, y, x, ret = INT_MIN;
+char a[50][50];
 int visited[50][50];
 int dy[4] = {1, 0, -1, 0};
 int dx[4] = {0, 1, 0, -1};
-int bfs(int sy, int sx){
-    memset(visited, 0, sizeof(visited));
-    queue<pair<int, int>> q;
-    int dis = 0;
-    visited[sy][sx] = 1;
-    q.push({sy, sx});
-    while (q.size()){
-        tie(y, x) = q.front();
-        q.pop();
-        for(int i = 0; i < 4; ++i){
-            ny = y + dy[i];
-            nx = x + dx[i];
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-            if(adj[ny][nx] && !visited[ny][nx]){
-                visited[ny][nx] = visited[y][x] + 1;
-                q.push({ny, nx});
-                dis = max(dis, visited[ny][nx]);
-            }
-        }
-    }
-    return dis - 1;
-}
+vector<pair<int, int>> v;
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
     for(int i = 0; i < n; ++i){
         for(int j = 0; j < m; ++j){
-            cin >> c;
-            if(c == 'L'){
-                adj[i][j] = 1;
-            }
+            cin >> a[i][j];
+            if(a[i][j] == 'L')
+                v.push_back({i, j});
         }
     }
-    for(int i = 0; i < n; ++i){
-        for(int j = 0; j < m; ++j){
-            if(adj[i][j] == 1){
-                ret = max(ret, bfs(i, j));
+
+    for(pair<int, int> b : v){
+        memset(visited, 0, sizeof(visited));
+        int sy = b.first;
+        int sx = b.second;
+        visited[sy][sx] = 1;
+        queue<pair<int, int>> q;
+        q.push({sy, sx});
+        int cnt = 0;
+        while (q.size()){
+            tie(y, x) = q.front();
+            q.pop();
+            for(int i = 0; i < 4; ++i){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx]) continue;
+                if(a[ny][nx] == 'L'){
+                    visited[ny][nx] = visited[y][x] + 1;
+                    cnt = max(cnt, visited[ny][nx]);
+                    q.push({ny, nx});
+                }
             }
         }
+        ret = max(ret, cnt-1);
     }
     cout << ret << "\n";
+    return 0;
 }
