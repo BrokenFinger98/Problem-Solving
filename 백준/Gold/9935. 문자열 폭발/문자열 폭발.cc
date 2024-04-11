15877c544e9f092cdbda0bb66920372d8136ad26
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-04-12 01:43:50 +0900

    [Gold IV] Title: 문자열 폭발, Time: 48 ms, Memory: 8756 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,18 +1,33 @@
 #include<bits/stdc++.h>
-using namespace std; 
-string s, boom, ret;
+using namespace std;
+string s, t, ret;
+stack<char> st;
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL); cout.tie(NULL);
-    cin >> s >> boom;
+    cin >> s >> t;
     for(char a : s){
-        ret += a;
-        if(ret.size() >= boom.size() && ret.substr(ret.size() - boom.size()) == boom)
-            ret.erase(ret.size() - boom.size(), boom.size());
+        st.push(a);
+        if(st.size() >= t.size() && st.top() == t[t.size()-1]){
+            string tmp = "";
+            for(char c : t){
+                tmp += st.top();
+                st.pop();
+            }
+            reverse(tmp.begin(), tmp.end());
+            if(tmp != t){
+                for(char c : tmp)
+                    st.push(c);
+            }
+        }
     }
-    if(ret.size())
+    if(st.size() == 0){
+        cout << "FRULA" << "\n";
+    }else{
+        while (st.size()){
+            ret += st.top(); st.pop();
+        }
+        reverse(ret.begin(), ret.end());
         cout << ret << "\n";
-    else
-        cout << "FRULA" << "\n"; 
+    }
     return 0;
 }
+
