22f693656b3219f1f8965ca3ef745fbafab832a2
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-04-16 18:15:49 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -1,34 +1,35 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std; 
 int n, ret = INT_MIN;
 string s;
-vector<char> oper_str;
 vector<int> num;
-int oper(char a, int b, int c){
-    if(a == '+') return b + c;
-    if(a == '-') return b - c;
-    if(a == '*') return b * c;
+vector<char> oper;
+int cal(int a, int b, char c){
+    if(c == '+') return a+b;
+    if(c == '-') return a-b;
+    if(c == '*') return a*b;
 }
 void go(int here, int sum){
     if(here == num.size()-1){
         ret = max(ret, sum);
         return;
     }
-    go(here+1, oper(oper_str[here], sum, num[here+1]));
+    go(here + 1, cal(sum, num[here+1], oper[here]));
     if(here + 2 <= num.size()-1){
-        int tmp = oper(oper_str[here+1], num[here+1], num[here+2]);
-        go(here+2, oper(oper_str[here], sum, tmp));
+        int tmp = cal(num[here+1], num[here+2], oper[here+1]);
+        go(here + 2, cal(sum, tmp, oper[here]));
     }
     return;
 }
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); 
+    cout.tie(NULL);   
     cin >> n;
-    cin >> s;
-    for(int i = 0; i < n; ++i){
-        if(i%2 == 0) num.push_back(s[i] - '0');
-        else oper_str.push_back(s[i]);
+    cin >> s; 
+    for(int i = 0; i < n; i++){
+        if(i % 2 == 0) num.push_back(s[i] - '0');
+        else oper.push_back(s[i]);
     }
     go(0, num[0]);
     cout << ret << "\n";
