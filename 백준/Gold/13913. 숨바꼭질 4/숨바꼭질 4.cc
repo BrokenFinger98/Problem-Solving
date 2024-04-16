6c0230114f0f0f884db513207e01c0500a252b9e
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-04-17 00:54:06 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 3700 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,34 +1,44 @@
 #include<bits/stdc++.h>
-using namespace std;
-#define prev aaaa
-const int maxS = 200000;
-int n, k, now;
-int visited[maxS];
-int prev[maxS];
+using namespace std; 
+const int MAX = 100001;
+int n, k, now, next, e;
+int visited[MAX+4], pre[MAX+4];
 queue<int> q;
-vector<int> v;
 int main(){
-    ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> n >> k;
+    if(n == k){
+        cout << 0 << "\n";
+        cout << n << "\n";
+        return 0;
+    }
     visited[n] = 1;
+    pre[n] = n;
     q.push(n);
     while (q.size()){
-        now  = q.front(); q.pop();
-        for(int next : {now+1, now-1, now*2}){
-            if(next < 0 || next >= maxS || visited[next]) continue;
-            visited[next] = visited[now] + 1;
-            prev[next] = now;
-            q.push(next);
+        now = q.front(); q.pop();
+        if(now == k)
+            break;
+        for(int next : {now-1, now+1, now*2}){
+            if(next < 0 || next > MAX) continue;
+            if(!visited[next]){
+                visited[next] = visited[now] + 1;
+                pre[next] = now;
+                q.push(next);
+            }
         }
     }
-    
-    for(int i = k; i != n; i = prev[i])
-        v.push_back(i);
-    v.push_back(n);
+
+    vector<int> v;
+    v.push_back(k);
+    for(int i = k; i != n; i = pre[i])
+        v.push_back(pre[i]);
     reverse(v.begin(), v.end());
 
-    cout << visited[k]-1 << "\n";
-    for(int a : v) cout << a << " ";
+    cout << visited[k] - 1 << "\n";
+    for(int a : v)
+        cout << a << " ";
+    cout << "\n";
     return 0;
 }
