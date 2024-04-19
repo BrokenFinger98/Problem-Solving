221d731805601190166836d04fa0616c01275aeb
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-04-19 17:36:57 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+string s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for(int i = 0; i < n; ++i){
+        cin >> s;
+        cout << s[0] << s[s.length()-1] << "\n";
+    }
+    return 0;
+}
