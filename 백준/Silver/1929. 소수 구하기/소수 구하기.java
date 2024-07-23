4d0ec36d284f672c3382ba07368cbbc83a5958ef
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-07-23 19:43:13 +0900

    [Silver III] Title: 소수 구하기, Time: 316 ms, Memory: 22572 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,34 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int M, N;
-    static ArrayList<Integer> a = new ArrayList<>();
-    static ArrayList<Integer> ret = new ArrayList<>();
+class Main {
+    static int N, M;
+    static boolean[] check;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        
-        for(int i = M; i <= N; ++i) a.add(i);
+        check = new boolean[N+1];
+        check[0] = true;
+        check[1] = true;
+        for (int i = 2; i*i<=N; i++) {
+            if(!check[i]){
+                for(int j = i+i; j <= N; j+=i) {
+                    check[j] = true;
+                }
+            }
+        }
 
-        for (int i = 0; i < a.size(); i++) {
-            if (checkPrime(a.get(i))) ret.add(a.get(i));
+        for(int i = M; i <= N; i++) {
+            if(!check[i]) bw.write(i + "\n");
         }
-        for (Integer a : ret) {
-            bw.write(a + "\n");
-        }
+
         bw.flush();
         bw.close();
         br.close();
-    }
-    static boolean checkPrime(int p){
-        if(p < 2) return false;
-        for (int i = 2; i*i <= p ; i++) {
-            if(p % i == 0) return false;
-        }
-        return true;
     }
 }
