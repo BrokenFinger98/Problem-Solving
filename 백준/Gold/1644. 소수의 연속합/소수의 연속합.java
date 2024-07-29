48026ea71ed2a2f73286cc4afa92d91dc0c9df91
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-07-29 13:45:09 +0900

    [Gold III] Title: 소수의 연속합, Time: 364 ms, Memory: 29412 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,31 +1,65 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N, ret;
-    static ArrayList<Integer> a = new ArrayList<>();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
+    static int N;
 
-        for (int i = 2; i <= N; ++i) {
-            int flag = 1;
-            for (int j = 2; j * j <= i; j++) {
-                if (i % j == 0) {
-                    flag = 0;
-                    break;
-                }
+    // 해당 인덱스가 소수이면 false, 아니면 true를 저장하는 배열
+    // boolean의 default값은 false이기 때문에
+    static boolean[] checkPrime;
+
+    // 소수를 저장하는 배열
+    static int[] prime = new int[400004];
+    static int L, R;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+
+        // N까지 소수 일 수 있으므로 N+1 사이즈로 배열 생성
+        checkPrime = new boolean[N+1];
+
+        // 0, 1은 소수가 아님
+        checkPrime[0] = true;
+        checkPrime[1] = true;
+
+        for(int i = 2; i * i <= N; ++i){
+            if(!checkPrime[i]){
+                for(int j = i + i; j <= N; j += i)
+                    checkPrime[j] = true;
             }
-            if (flag == 1) a.add(i);
         }
-        for (int i = 0; i < a.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < a.size() ; j++) {
-                sum += a.get(j);
-                if(sum == N) ++ret;
-                if(sum > N) break;
+
+        int k = 1;
+        for(int i = 2; i <= N; ++i){
+            if(!checkPrime[i]){
+                prime[k] = prime[k-1] + i;
+                ++k;
             }
         }
-        System.out.println(ret);
+
+        L = 0;
+        R = 1;
+        int sum = 0;
+        int result = 0;
+        while (true){
+            if(R <= L || R >= k){
+                break;
+            }
+            sum = prime[R] - prime[L];
+            if(sum < N) ++R;
+            else if(sum > N) ++L;
+            else{
+                result++;
+                ++R;
+                ++L;
+            }
+        }
+        bw.write(Integer.toString(result) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
