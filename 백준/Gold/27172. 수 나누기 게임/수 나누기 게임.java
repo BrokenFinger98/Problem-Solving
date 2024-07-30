b232dc92cc80bad8bad99920868c4ef6f3f9c7ac
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-07-30 17:55:57 +0900

    [Gold V] Title: 수 나누기 게임, Time: 844 ms, Memory: 75692 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -1,37 +1,41 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+import java.lang.*;
 
 public class Main {
     static int MAX_CARD_SIZE = 10000004;
     static int MAX_SIZE = 100004;
     static int N;
     static int[] player = new int[MAX_SIZE];
-    static int[] score = new int[MAX_CARD_SIZE];
     static boolean[] card = new boolean[MAX_CARD_SIZE];
-    public static void main(String[] args) throws IOException {
+    static int[] score = new int[MAX_CARD_SIZE];
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < N; ++i){
             player[i] = Integer.parseInt(st.nextToken());
             card[player[i]] = true;
         }
+
         for(int i = 0; i < MAX_CARD_SIZE; ++i){
-            if(card[i])
-                for (int j = i*2; j < MAX_CARD_SIZE; j+=i) {
+            if(card[i]){
+                for(int j = i * 2; j < MAX_CARD_SIZE; j += i){
                     if(card[j]){
                         score[i]++;
                         score[j]--;
                     }
                 }
+            }
         }
 
-        for (int i = 0; i < N; i++) {
-            System.out.print(score[player[i]] + " ");
+        for(int i = 0; i < N; ++i){
+            sb.append(score[player[i]]).append(" ");
         }
-        System.out.println();
+        System.out.println(sb.toString());
         br.close();
     }
 }
