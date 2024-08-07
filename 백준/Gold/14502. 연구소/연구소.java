b26974e468fc45c1bb1d08ba3a5058b4792bc484
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-08-07 14:20:16 +0900

    [Gold IV] Title: 연구소, Time: 340 ms, Memory: 92620 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -65,19 +65,19 @@ public class Main {
     }
 
     static void bfs() {
-        Deque<Pair> stack = new ArrayDeque<>();
+        Deque<Pair> queue = new ArrayDeque<>();
         for (Pair pair : virus) {
-            stack.push(pair);
+            queue.offer(pair);
         }
-        while (!stack.isEmpty()) {
-            Pair now = stack.pop();
+        while (!queue.isEmpty()) {
+            Pair now = queue.poll();
             for (int i = 0; i < 4; ++i) {
                 int ny = now.y + dy[i];
                 int nx = now.x + dx[i];
                 if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
                 if (mp1[ny][nx] == 0) {
                     mp1[ny][nx] = 2;
-                    stack.push(new Pair(ny, nx));
+                    queue.push(new Pair(ny, nx));
                 }
             }
         }
