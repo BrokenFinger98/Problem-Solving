a29e20cb2e2554189cc139f5b48543222b1aaccf
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-08-19 01:19:21 +0900

    [D2] Title: 나무 높이, Time: 110 ms, Memory: 18,424 KB -BaekjoonHub

---
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Solution {
+    static int T, N, result, maxHeight, height;
+    static int[] trees = new int[104];
+    static int oddCount, evenCount;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb;
+
+        T = Integer.parseInt(st.nextToken());
+        for(int t = 1; t <= T; ++t){
+            sb = new StringBuilder();
+            trees = new int[104];
+            result = 0;
+            maxHeight = 0;
+            N = Integer.parseInt(br.readLine());
+
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; ++i){
+                trees[i] = Integer.parseInt(st.nextToken());
+                maxHeight = Math.max(trees[i], maxHeight);
+            }
+            for (int i = 0; i < N; i++) {
+                if(trees[i] == maxHeight) continue;
+                height = maxHeight - trees[i];
+                if(height % 2 == 0){
+                    evenCount += height/2;
+                }else{
+                    oddCount++;
+                    evenCount += (height-1)/2;
+                }
+            }
+
+            while (evenCount != 0 || oddCount != 0){
+                ++result;
+                if(result % 2 == 0){
+                    if(evenCount != 0){
+                        evenCount--;
+                    }
+                }else{
+                    if(oddCount == 0){
+                        if(evenCount > 1){
+                            evenCount--;
+                            oddCount++;
+                        }
+                    }else{
+                        oddCount--;
+                    }
+                }
+            }
+
+            sb.append("#").append(t).append(" ").append(result);
+            System.out.println(sb.toString());
+        }
+        br.close();
+    }
+}
