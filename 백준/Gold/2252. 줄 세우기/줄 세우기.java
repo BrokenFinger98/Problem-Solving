7ddd507f7eb97dc0e46d3934a5d7b8298b2fc64f
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-08-27 14:17:41 +0900

    [Gold III] Title: 줄 세우기, Time: 672 ms, Memory: 51360 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] inDegree;
+    static List<Integer>[] list;
+    static Queue<Integer> queue = new ArrayDeque<>();
+    static List<Integer> result = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        inDegree = new int[N+1];
+        list = new List[N+1];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            list[from].add(to);
+            inDegree[to]++;
+        }
+        for (int i = 1; i < inDegree.length; i++) {
+            if(inDegree[i] == 0) queue.offer(i);
+        }
+
+        while (!queue.isEmpty()){
+            int now = queue.poll();
+            result.add(now);
+            for (Integer i : list[now]) {
+                inDegree[i]--;
+                if(inDegree[i] == 0) queue.offer(i);
+            }
+        }
+        for (Integer i : result) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        br.close();
+    }
+}
