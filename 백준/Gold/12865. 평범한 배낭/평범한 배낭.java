b30388b90d4f0dc6abed9dc532801823797d27c0
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-09-30 16:26:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 216 ms, Memory: 93172 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,40 +1,73 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static int[][] dp;
-    static int[] W;
-    static int[] V;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	private static class Thing implements Comparable<Thing>{
+		int w;
+		int v;
+		public Thing(int w, int v) {
+			super();
+			this.w = w;
+			this.v = v;
+		}
+		
+		public int compareTo(Thing o) {
+			return this.w - o.w;
+		}
+	}
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        dp = new int[N+1][K+1];
-        W = new int[N+1];
-        V = new int[N+1];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
-        }
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 0; j <= K; j++) {
-                if(j >= W[i]){
-                    dp[i][j] = Math.max(dp[i-1][j-W[i]] + V[i], dp[i-1][j]);
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        
-        System.out.println(dp[N][K]);
-        br.close();
-    }
+		Thing[] things = new Thing[n];
+		int[] weight = new int[100001];
+		long[][] dp = new long[n][k+1];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			
+			things[i] = new Thing(w, v);
+			weight[w] = v;
+		}
+		
+		Arrays.sort(things);
+		
+		long max = 0;
+		for (int j = 1; j < k + 1; j++) {
+			if (j < things[0].w)
+				continue;
+			dp[0][j] = things[0].v;
+			max = things[0].v;
+		}
+		
+		
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < k+1; j++) {
+				if (j >= things[i].w) {
+					dp[i][j] = Math.max(dp[i-1][j], things[i].v + dp[i-1][j - things[i].w]);
+				} else {
+					dp[i][j] = dp[i-1][j];
+				}
+				max = Math.max(max, dp[i][j]);
+			}
+		}
+//		
+//		for (int i = 0; i < n; i++) {
+//			for (int j = 1; j < k+1; j++) {
+//				System.out.print(dp[i][j]);
+//			}
+//			System.out.println();
+//		}
+		System.out.println(max);
+	}
+
 }
