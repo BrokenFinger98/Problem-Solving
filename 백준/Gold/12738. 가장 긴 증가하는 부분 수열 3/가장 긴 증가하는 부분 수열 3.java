3a44399f14655bf081cd539528b9d0529d67c836
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-02 00:37:17 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 464 ms, Memory: 165580 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, num, answer, index;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        dp = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num = Integer.parseInt(st.nextToken());
+            index = lowerBound(num);
+            if(dp[index] == 0) ++answer;
+            dp[index] = num;
+        }
+        System.out.println(answer);
+        br.close();
+    }
+
+    static int lowerBound(int num) {
+        int low = 0;
+        int height = answer;
+        int mid;
+        while(low < height){
+            mid = (low + height)/2;
+            if(dp[mid] < num)
+                low = mid + 1;
+            else
+                height = mid;
+        }
+        return low;
+    }
+}
