5b9a7a2d7fcd031780dc5376494249df1973fbdd
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-02 12:41:29 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 420 ms, Memory: 121628 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,29 +13,18 @@ public class Main {
         N = Integer.parseInt(br.readLine());
         dp = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
+
+        dp[0] = Integer.parseInt(st.nextToken());
+        answer = 1;
+        for (int i = 1; i < N; i++) {
             num = Integer.parseInt(st.nextToken());
-            index = lowerBound(num);
-            if(dp[index] == 0) ++answer;
+            index = Arrays.binarySearch(dp, 0, answer, num);
+            if (index < 0) index = -(index + 1);
             dp[index] = num;
+            if(index == answer) ++answer;
         }
         System.out.println(answer);
         br.close();
     }
-
-    static int lowerBound(int num) {
-        int low = 0;
-        int height = answer;
-        int mid;
-        while(low < height){
-            mid = (low + height)/2;
-            if(dp[mid] < num)
-                low = mid + 1;
-            else
-                height = mid;
-        }
-        return low;
-    }
-
 }
 
