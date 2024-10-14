84189ae318fe06f21b737fb54edfba3ac5fcfe9b
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-14 16:44:31 +0900

    [Gold I] Title: 구간 합 구하기, Time: 468 ms, Memory: 109072 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
     static int N, M, K;
     static long[] tree;
@@ -11,53 +12,49 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        input = new long[N];
-        tree = new long[N*4];
-        for (int i = 0; i < N; i++) {
+        tree = new long[N+1];
+        input = new long[N+1];
+        for (int i = 1; i <= N; i++) {
             input[i] = Long.parseLong(br.readLine());
+            update(tree, i, input[i]);
         }
-        init(0, N-1, 1);
-        for (int i = 0; i < M+K; i++) {
+        M += K;
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
-            if(a == 1){
+            if (a == 1) {
                 int b = Integer.parseInt(st.nextToken());
                 long c = Long.parseLong(st.nextToken());
-                long diff = c - input[b-1];
-                input[b-1] = c;
-                update(0, N-1, 1, b-1, diff);
+                long dif = c - input[b];
+                input[b] = c;
+                update(tree, b, dif);
             }else{
                 int b = Integer.parseInt(st.nextToken());
                 int c = Integer.parseInt(st.nextToken());
-                long sum = sum(0, N-1, 1, b-1, c-1);
-                sb.append(sum).append("\n");
+                long totalSum = sum(tree, c) - sum(tree, b-1);
+                sb.append(totalSum).append("\n");
             }
         }
         System.out.print(sb);
-        br.close();
-    }
-    static long init(int start, int end, int node){
-        if(start == end) return tree[node] = input[start];
-        int mid = (start + end)/2;
-        return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
     }
 
-    static long sum(int start, int end, int node, int left, int right){
-        if(left > end || right < start) return 0;
-        if(left <= start && end <= right) return tree[node];
-        int mid = (start + end)/2;
-        return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+    private static long sum(long[] tree, int i) {
+        long answer = 0;
+        while (i > 0){
+            answer += tree[i];
+            i -= (i & -i);
+        }
+        return answer;
     }
-    static void update(int start, int end, int node, int index, long diff){
-        if(index < start || index > end) return;
-        tree[node] += diff;
-        if(start == end) return;
-        int mid = (start + end)/2;
-        update(start, mid, node * 2, index, diff);
-        update(mid + 1, end, node * 2 + 1, index , diff);
+
+    private static void update(long[] tree, int i, long dif) {
+        while (i < tree.length){
+            tree[i] += dif;
+            i += (i & -i);
+        }
     }
 }
-
