86e3ca70f65bd0d1cdde699cfa24a3093a4984a4
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-21 16:41:26 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 696 ms, Memory: 66268 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
@@ -1,54 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, a, b;
-    static int[] input;
-    static int[][] tree;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        tree = new int[N*4][2];
-        input = new int[N];
-        for (int i = 0; i < N; i++) {
-            input[i] = Integer.parseInt(br.readLine());
-        }
-        init(0, N-1, 1);
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            int[] result = query(0, N-1, 1, a-1, b-1);
-            sb.append(result[0]).append(" ").append(result[1]).append("\n");
-        }
-        System.out.print(sb);
-        br.close();
-    }
+	
+	private static int n;
+	private static int[] arr, maxTree, minTree;
 
-    static int[] query(int start, int end, int node, int left, int right) {
-        if(left > end || right < start) return new int[]{1_000_000_000, 1};
-        if(left <= start && right >= end) return new int[]{tree[node][0], tree[node][1]};
-        int mid = (start + end) / 2;
-        int leftChild[] = query(start, mid, node * 2, left, right);
-        int rightChild[] = query(mid + 1, end, node * 2 + 1, left, right);
-        return new int[]{Math.min(leftChild[0], rightChild[0]), Math.max(leftChild[1], rightChild[1])};
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		arr = new int[n];
+		
+		maxTree = new int[4 * n];
+		minTree = new int[4 * n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		makeTree(1, 0, n - 1);
 
-    static void init(int start, int end, int node){
-        if(start == end) {
-            tree[node][0] = input[start];
-            tree[node][1] = input[start];
-            return;
-        }
-        int mid = (start + end) / 2;
-        init(start, mid, node * 2);
-        init(mid + 1, end, node * 2 + 1);
-        tree[node][0] = Math.min(tree[node*2][0], tree[node*2 + 1][0]);
-        tree[node][1] = Math.max(tree[node*2][1], tree[node*2 + 1][1]);
-    }
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int left = Integer.parseInt(st.nextToken())-1;
+			int right = Integer.parseInt(st.nextToken())-1;
+			
+			int max = getMax(1, 0, n-1, left, right);
+			int min = getMin(1, 0, n-1, left, right);
+			
+			sb.append(min + " " + max + "\n");
+		}
+		System.out.println(sb);
+	}
+	
+	public static void makeTree(int node, int nodeLeft, int nodeRight) {
+		if (nodeLeft == nodeRight) {
+			maxTree[node] = arr[nodeLeft];
+			minTree[node] = arr[nodeLeft];
+			return;
+		}
+		
+		int mid = (nodeLeft + nodeRight) / 2;
+		
+		makeTree(2 * node, nodeLeft, mid);
+		makeTree(2 * node + 1, mid + 1, nodeRight);
+		
+		maxTree[node] = Math.max(maxTree[2 * node], maxTree[2 * node + 1]);
+		minTree[node] = Math.min(minTree[2 * node], minTree[2 * node + 1]);
+	}
+	
+	public static int getMax(int node, int s, int e, int ts, int te) {
+		if (s > te || e < ts) return 1;
+		else if (ts <= s && e <= te) return maxTree[node];
+		
+		int mid = (s + e) / 2;
+		
+		int left = getMax(2 * node, s, mid, ts, te);
+		int right = getMax(2 * node + 1, mid + 1, e, ts, te);
+		
+		return Math.max(left, right);
+	}
+	
+	public static int getMin(int node, int s ,int e, int ts, int te) {
+		if (s > te || e < ts) return 1_000_000_001;
+		else if (ts <= s && e <= te) return minTree[node];
+		
+		int mid = (s + e) / 2;
+		
+		int left = getMin(2 * node , s, mid, ts, te);
+		int right = getMin(2 * node + 1, mid + 1, e, ts, te);
+		
+		return Math.min(left, right);
+	}
 }
