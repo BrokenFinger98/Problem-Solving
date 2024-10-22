ef5dc879e589ebdaca09378ac4dbec073d2cc7d9
Sunwoo Yu <dbtjsdn980818@gmail.com>
2024-10-22 20:29:26 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 432 ms, Memory: 112348 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -4,90 +4,68 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static int n;
-	private static int[] arr;
-	private static long[] mulTree;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		arr = new int[n];
-		
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		mulTree = new long[4*n];
-		makeTree(1, 0, n - 1);
-		
-		for (int i = 0; i < m + k; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			int query = Integer.parseInt(st.nextToken());
-			
-			if (query == 1) {
-				int left = Integer.parseInt(st.nextToken())-1;
-				int right = Integer.parseInt(st.nextToken());
-				update(1, 0, n-1, left, right);
-			} else {
-				int left = Integer.parseInt(st.nextToken())-1;
-				int right = Integer.parseInt(st.nextToken())-1;
-				
-				long answer = getMul(1, 0, n-1, left, right);
-				sb.append(answer + "\n");
-			}
-		}
-		System.out.println(sb);
-	}
-	
-	public static void makeTree(int idx, int s, int e) {
-		if (s == e) {
-			mulTree[idx] = arr[s];
-			return;
-		}
-		
-		int mid = (s + e) / 2;
-		
-		makeTree(2 * idx, s, mid);
-		makeTree(2 * idx + 1, mid + 1, e);
-		
-		mulTree[idx] = ((mulTree[2 * idx] % 1_000_000_007) * (mulTree[2 * idx + 1] % 1_000_000_007)) % 1_000_000_007;
-	}
-	
-	public static void update(int idx, int ts, int te, int target, int value) {
-		if (target < ts || target > te) {
-			return;
-		}
-		
-		if (ts == te) {
-			mulTree[idx] = value;
-			return;
-		}
-		
-		int mid = (ts + te) / 2;
-		
-		update(2 * idx, ts, mid, target, value);
-		update(2 * idx + 1, mid + 1, te, target, value);
-		
-		mulTree[idx] = ((mulTree[2 * idx] % 1_000_000_007) * (mulTree[2 * idx + 1] % 1_000_000_007)) % 1_000_000_007;
-	}
-	
-	public static long getMul(int idx, int s, int e, int ts, int te) {
-		if (s > te || e < ts) return 1;
-		else if (ts <= s && e <= te) return mulTree[idx];
-		
-		int mid = (s + e) / 2;
-		
-		long left = getMul(2 * idx, s, mid, ts, te);
-		long right = getMul(2 * idx + 1, mid + 1, e, ts, te);
-		
-		return ((left%1_000_000_007) * (right%1_000_000_007)) % 1_000_000_007;
-	}
+    static int N, M, K;
+    static long[] tree;
+    static int[] input;
+    static final int MOD = 1_000_000_007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        input = new int[N];
+        tree = new long[N * 4];
+
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(br.readLine());
+        }
+        init(0, N-1, 1);
+        M += K;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if(a == 1){
+                if(input[b-1] == 0){
+                    input[b-1] = c;
+                    init(0, N-1, 1);
+                }else{
+                    update(0, N-1, 1, b-1, c);
+                    input[b-1] = c;
+                }
+            }else{
+                sb.append(sum(0, N-1, 1, b-1, c-1)).append("\n");
+            }
+        }
+        System.out.print(sb);
+        br.close();
+    }
+
+    private static long sum(int start, int end, int node, int left, int right) {
+        if(left > end || right < start) return 1;
+        if(left <= start && end <= right) return tree[node];
+        int mid = (start + end) / 2;
+        return (sum(start, mid, node * 2, left, right) * sum(mid + 1, end, node * 2 + 1, left, right)) % MOD;
+    }
+
+    private static void update(int start, int end, int node, int index, int value) {
+        if(start > index || end < index) return;
+        tree[node] /= input[index];
+        tree[node] *= value;
+        tree[node] %= MOD;
+        if(start == end) return;
+        int mid = (start + end) / 2;
+        update(start, mid, node * 2, index, value);
+        update(mid + 1, end, node * 2 + 1, index, value);
+        tree[node] = (tree[node * 2] * tree[node * 2 + 1]) % MOD;
+    }
+
+    private static long init(int start, int end, int node) {
+        if(start == end) return tree[node] = input[start];
+        int mid = (start + end) / 2;
+        return tree[node] = (init(start, mid, node * 2) * init(mid + 1, end, node * 2 + 1)) % MOD;
+    }
 }
