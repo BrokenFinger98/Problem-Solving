e5c70aaff1551ea20ea6f1453d6c75b29f1eb13a
Sunwoo Yu <dbtjsdn980818@gmail.com>
2025-09-04 16:32:37 +0900

    [Silver III] Title: 소수 구하기, Time: 184 ms, Memory: 25988 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,32 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    static int N, M;
-    static boolean[] check;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        check = new boolean[N+1];
-        check[0] = true;
-        check[1] = true;
-        for (int i = 2; i*i<=N; i++) {
-            if(!check[i]){
-                for(int j = i+i; j <= N; j+=i) {
-                    check[j] = true;
-                }
-            }
-        }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
 
-        for(int i = M; i <= N; i++) {
-            if(!check[i]) bw.write(i + "\n");
-        }
+		boolean[] check = new boolean[N+1];
+		check[0] = true;
+		check[1] = true;
 
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		for (int i = 2; i * i <= N; ++i) {
+			if (!check[i]) {
+				for (int j = i + i; j <= N; j += i) {
+					check[j] = true;
+				}
+			}
+		}
+
+		for (int i = M; i <= N; ++i) {
+			if (!check[i]) {
+				sb.append(i + "\n");
+			}
+		}
+
+		System.out.print(sb);
+		br.close();
+	}
 }
