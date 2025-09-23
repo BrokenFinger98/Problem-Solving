57e72d848db07c7a86c59bb8ead88bfd267a47db
Sunwoo Yu <dbtjsdn980818@gmail.com>
2025-09-23 16:52:50 +0900

    [Gold IV] Title: 최단경로, Time: 1000 ms, Memory: 121852 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,76 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	private static final int INF = (int)1e9;
-	private static int[] d;
-	private static ArrayList<ArrayList<int[]>> list = new ArrayList<ArrayList<int[]>>();
+	private static class Node {
+		int u, weight;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int v = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		
-		d = new int[v+1];
-		Arrays.fill(d, INF);
-		
-		for (int i = 0; i < v+1; i++) {
-			list.add(new ArrayList<>());
-		}
-		
-		int k = Integer.parseInt(br.readLine());
-		for (int i = 0; i < e; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int dis = Integer.parseInt(st.nextToken());
-			
-			list.get(from).add(new int[] {to, dis});
-		}
-		
-		dijkstra(k);
-		
-		for (int i = 1; i < v+1; i++) {
-			if (d[i] == INF) {
-				System.out.println("INF");
-			}else {
-				System.out.println(d[i]);
-			}
+		public Node(int u, int weight){
+			this.u = u;
+			this.weight = weight;
 		}
 	}
 
-	private static void dijkstra(int start) {
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-		
-		pq.add(new int[] {start, 0});
-		d[start] = 0;
-		
-		while (!pq.isEmpty()) {
-			int[] p = pq.poll();
-			
-			int now = p[0];
-			int dis = p[1];
-			
-			if (d[now] < dis) continue;
-			
-			for (int i = 0; i < list.get(now).size(); i++) {
-				int cost = dis + list.get(now).get(i)[1];
-				
-				if (cost < d[list.get(now).get(i)[0]]) {
-					d[list.get(now).get(i)[0]] = cost;
-					pq.add(new int[] {list.get(now).get(i)[0], cost});
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(br.readLine());
+
+		List<Node>[] nodes = new List[V+1];
+		int[] dist = new int[V + 1];
+		for(int i = 1; i <= V; ++i) {
+			dist[i] = Integer.MAX_VALUE;
+			nodes[i] = new ArrayList<>();
+		}
+
+		for(int i = 0; i < E; ++i){
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			Node node = new Node(to, weight);
+			nodes[from].add(node);
+		}
+
+		dijkstra(start, dist, nodes);
+
+		for(int i = 1; i <= V; ++i){
+			if(dist[i] == Integer.MAX_VALUE) System.out.println("INF");
+			else System.out.println(dist[i]);
+		}
+
+		br.close();
+	}
+
+	private static void dijkstra(int start, int[] dist, List<Node>[] nodes) {
+		dist[start] = 0;
+		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+		pq.offer(new Node(start, 0));
+		while(!pq.isEmpty()) {
+			Node now = pq.poll();
+			int here = now.u;
+			int here_dist = now.weight;
+			if(dist[here] != here_dist) continue;
+			for(Node node : nodes[here]) {
+				int to = node.u;
+				int weight = node.weight;
+				if (dist[to] > dist[here] + weight) {
+					dist[to] = dist[here] + weight;
+					pq.offer(new Node(to, dist[to]));
 				}
 			}
 		}
 	}
-
 }
